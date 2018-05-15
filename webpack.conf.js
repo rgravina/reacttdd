1b683d6abbb3fc71559082a8fecb967ecf77b239
Robert Gravina <rgravina@pivotal.io>
2018-05-15 11:16:50 +0900

    Add build task.

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,5 +1,11 @@
 const path = require('path');
 
-let config = {entry: './src/index.js'};
+let config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+};
 
 module.exports = config;
