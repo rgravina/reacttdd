a3934a76c25d9f404283b8697285b123cc63189f
Robert Gravina <rgravina@pivotal.io>
2018-05-16 10:07:37 +0900

    Use functional component for App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-export default class App extends React.Component {
-    render() {
-        return (<div>Hello React App!</div>)
-    }
+export default () => {
+    return <div>Hello React App!</div>
 }
